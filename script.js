db11331ec1ae01e5f504292c88f7d1f3bf80d573
Blake Giunta <blakegiunta@gmail.com>
2023-12-15 17:21:19 -0600

    Updated script for better sped test accuracy

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 function startTest() {
-  document.getElementById('results').innerHTML = 'Testing...';
-  const startTime = (new Date()).getTime();
+  document.getElementById('results').innerHTML = 'Testing...'
+  let numberOfTests = 3 // Number of tests to average
+  let completedTests = 0
+  let totalSpeed = 0
 
-  fetch('/largefile')
-      .then(response => response.blob())
-      .then(data => {
-          const endTime = (new Date()).getTime();
-          const duration = (endTime - startTime) / 1000;
-          const bitsLoaded = data.size * 8;
-          const speedBps = (bitsLoaded / duration).toFixed(2);
-          const speedKbps = (speedBps / 1024).toFixed(2);
-          const speedMbps = (speedKbps / 1024).toFixed(2);
+  function testDownload() {
+    const startTime = new Date().getTime()
 
-          document.getElementById('results').innerHTML = 
-              `Your speed: ${speedMbps} Mbps (${speedKbps} Kbps)`;
+    fetch('/largefile.dat')
+      .then((response) => response.blob())
+      .then((data) => {
+        const endTime = new Date().getTime()
+        const duration = (endTime - startTime) / 1000 // Duration in seconds
+        const bitsLoaded = data.size * 8 // Total bits loaded
+        const speedBps = bitsLoaded / duration // Speed in bits per second
+        totalSpeed += speedBps
+        completedTests++
+
+        if (completedTests < numberOfTests) {
+          testDownload() // Run next test
+        } else {
+          const averageSpeedBps = totalSpeed / numberOfTests
+          const averageSpeedMbps = (averageSpeedBps / (1024 * 1024)).toFixed(2) // Convert to Mbps
+
+          document.getElementById('results').innerHTML = `Average speed: ${averageSpeedMbps} Mbps`
+        }
       })
-      .catch(error => {
-          document.getElementById('results').innerHTML = 'Error in speed test';
-      });
+      .catch((error) => {
+        document.getElementById('results').innerHTML = 'Error in speed test'
+      })
+  }
+
+  testDownload()
 }
